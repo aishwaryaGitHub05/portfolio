45c49e078cff8882063c470374bee636c12cdeb3
aishwarya@nextontop.com <aishwarya@nextontop.com>
2020-01-09 22:54:29 +0530

    files added to repo

---
+++ build/precache-manifest.8126dee0d0d90b40a92482b9b345fb6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc94cace226bf3603135650d731c229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da7f54db9aaaf593be2",
+    "url": "/static/css/main.4d2393a9.chunk.css"
+  },
+  {
+    "revision": "80c74617efc9bc052272",
+    "url": "/static/js/2.cb92b439.chunk.js"
+  },
+  {
+    "revision": "6da7f54db9aaaf593be2",
+    "url": "/static/js/main.0f58d34f.chunk.js"
+  },
+  {
+    "revision": "6c33cc3f50f6d904f59c",
+    "url": "/static/js/runtime-main.1b3ab917.js"
+  }
+]);
